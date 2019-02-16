5e94f153e6879dd77c726e59fcdb093e32319e50
kiddos <kiddo831007@gmail.com>
2019-02-16 08:18:41 +0800

    add spinner to choose collection

--- app/src/main/java/com/kiddos/nerdvocabs/QuizFragment.kt
+++ app/src/main/java/com/kiddos/nerdvocabs/QuizFragment.kt
@@ -7,9 +7,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.RadioGroup
-import android.widget.TextView
+import android.widget.*
 
 
 /**
@@ -17,20 +15,59 @@ import android.widget.TextView
  *
  */
 class QuizFragment : Fragment() {
+    private lateinit var helper: WordDataHelper
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView = inflater.inflate(R.layout.fragment_quiz, container, false)
-        val helper = WordDataHelper(context!!)
-        val question = helper.getQuestion()
-        prepareQuestion(rootView, question)
+        helper = WordDataHelper(context!!)
 
         rootView.findViewById<Button>(R.id.btnNext).setOnClickListener {
-            val nextQuestion = helper.getQuestion()
-            prepareQuestion(rootView, nextQuestion)
+            rootView.findViewById<RadioButton>(R.id.rb1).isSelected = false
+            rootView.findViewById<RadioButton>(R.id.rb2).isSelected = false
+            rootView.findViewById<RadioButton>(R.id.rb3).isSelected = false
+            rootView.findViewById<RadioButton>(R.id.rb4).isSelected = false
+            chooseQuestion(rootView)
         }
+
+        initCollection(rootView)
+
+        chooseQuestion(rootView)
         return rootView
     }
 
-    fun prepareQuestion(rootView: View, question: Question?) {
+    private fun chooseQuestion(rootView: View) {
+        val spinner = rootView.findViewById<Spinner>(R.id.spCollection)
+        val selected = spinner.selectedItem.toString()
+        Log.d("selected", selected)
+        if (selected.equals(resources.getString(R.string.my_words))) {
+            val words = helper.getWords()
+            val question = helper.getQuestion(words)
+            prepareQuestion(rootView, question)
+        } else {
+            val words = helper.loadFromAsset(selected)
+            val question = helper.getQuestion(words)
+            prepareQuestion(rootView, question)
+        }
+    }
+
+    private fun initCollection(rootView: View) {
+        val assets = context!!.assets.list("")
+        val collection = ArrayList<String>()
+        for (a : String in assets) {
+            if (a.endsWith(".txt")) {
+                collection.add(a)
+            }
+        }
+
+        val words = helper.getWords()
+        if (words.size >= 4) {
+            collection.add(resources.getString(R.string.my_words))
+        }
+        rootView.findViewById<Spinner>(R.id.spCollection).adapter = ArrayAdapter<String>(context!!,
+            android.R.layout.simple_spinner_dropdown_item, collection)
+    }
+
+    private fun prepareQuestion(rootView: View, question: Question?) {
         if (question != null) {
             rootView.findViewById<TextView>(R.id.tvAnswer).text = getString(R.string.choose_answer)
             rootView.findViewById<TextView>(R.id.tvAnswer).setTextColor(resources.getColor(R.color.colorAnswer))
